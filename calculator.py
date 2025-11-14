148280f1f17d1380602483af7f730f2092b7b7d3
Alexandr Math <arlan.suleymanov77@gmail.com>
2025-11-14 17:23:40 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -7,12 +7,6 @@ calculator.py
 One function per operation, in order.
 """
 # First example
-def add(a, b): 
-    return a + b
-
-def sub(a, b):
-    return a-b
-
 def mul(a, b):
     return a * b
 
@@ -20,11 +14,6 @@ def div(a, b):
     if a == 0:
         raise ZeroDivisionError
     return b/a
-
-def log(a, b):
-    if b<=0 or a == 1 or a <= 0:
-        raise ValueError
-    math.log(b,a)
 
 def exp(a, b):
     return a**b
@@ -43,5 +32,10 @@ def logarithm(a, b):
         raise ValueError
     return math.log(b, a)
 
-def exponent(a, b):
-    return a ** b
+def square_root(a):
+    if a < 0:
+        raise ValueError
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
